8b05f62fdc8eae6491b589048459c3b84a45b319
Yuyu <nutsongwat.cha@student.mahidol.ac.th>
2022-10-16 21:27:47 +0700

    [feat]: init

---
+++ src/pages/AddTask/AddTask.jsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { Button } from "antd";
+import { Cascader } from 'antd';
+import { Divider } from "antd";
+import { DatePicker } from 'antd';
+import { Input } from "antd";
+import { Tag } from "antd";
+import TextArea from "antd/lib/input/TextArea";
+import './AddTask.css';
+import './CustDes.css';
+
+export function AddTask() {
+  const [task, setTask] = React.useState("");
+  const [taskDes, setDes] = React.useState("");
+  // const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
+  const opt_remin = [{value:'',label:'None'}, {value:'30 min',label:'30 min'}, {value:'1 hours',label:'1 hours'}]
+  
+
+  const handleChange = (e) => {
+    setTask(e.target.value);
+  };
+  const handleDes = (e) => {
+    setDes(e.target.value);
+  };
+
+  const handleClick = () => { 
+    console.log('Button clicked');
+    console.log(task);
+    console.log(taskDes);
+  }
+
+  function onChange(date, dateString) {
+    console.log(date, dateString);
+  }
+
+  return (
+    <div className="add-task">
+
+      <Divider orientation="left">Title</Divider>
+      <Input
+        className="add-task-name"
+        placeholder="Add task"
+        value={task}
+        onChange={handleChange}
+      />
+      
+      <Divider orientation="left">Tag</Divider>
+      <div className="add-task-tag">
+        <Tag color="magenta">magenta</Tag>
+        <Tag color="red">red</Tag>
+        <Tag color="green">green</Tag>
+      </div>
+
+      <Divider orientation="left">Deadline</Divider>
+      <DatePicker onChange={onChange} />
+
+      <Divider orientation="left">Reminder</Divider>
+      <Cascader 
+        placeholder="Select Time"
+        options={opt_remin} 
+        changeOnSelect 
+      />
+
+      <Divider orientation="left">Description</Divider>
+
+      <span className="Controls">
+        <Button className="str-bold"><strong>B</strong></Button>
+        <Button className="str-em"><em>I</em></Button>
+        <Button className="str-u"><u>U</u></Button>
+      </span>
+
+      <TextArea
+        className="add-task-des"
+        // style={{ width: 337 }}
+        autoSize={{ minRows: 6, maxRows: 6 }}
+        placeholder="Add Description"
+        value={taskDes}
+        onChange={handleDes}
+      />
+
+      <Button className="cancel-button" type="defualt" onClick={handleClick} >Cancel</Button>
+      <Button className="add-button" type="defualt" onClick={handleClick} >Add</Button>
+
+    </div>
+  );
+}
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,25 +1,67 @@
 import React from 'react';
-import { Button } from 'antd';
 import './HomePage.css';
 
-export function HomePage() {
-  const [count, setCount] = React.useState(0);
+import { Divider } from "antd";
+import { Cascader } from "antd";
+import { Button } from "antd";
+import { Card } from "antd";
+import { Tag } from "antd";
+import { Checkbox } from 'antd';
+import TextArea from "antd/lib/input/TextArea";
 
-  const handleClick = () => { 
-    setCount(count + 1);
-    console.log('Button clicked');
-  }
+
+export function HomePage() {
+  const colors = [{value:'All',label:'All'}, {value:'TA',label:'TA'}, {value:'High School',label:'High School'}]
+  const { Meta } = Card;
+  const onChange = (e) => {
+    console.log(`checked = ${e.target.checked}`);
+  };
+
 
   return (
-    // for example
-    <div className="HomePage">
-      <div className="text">Remindเด้อ</div>
-      <div className="mt-2 text">{count}</div>
-      {/*
-        You can see document of each component in https://ant.design/components/overview/ 
-        eg. Button https://ant.design/components/button/
-      */}
-      <Button className="test-button" type="primary" onClick={handleClick} >ลองกดสิ</Button>
+    <div className="homepage">
+      <div className="text"> Remind-เด้อ </div>
+
+      <Divider orientation="left">Feature Future</Divider>
+      <TextArea
+        className="home-task-des"
+        autoSize={{ minRows: 2, maxRows: 2 }}
+        placeholder="Feature Future"
+      />
+      
+      <Divider orientation="left">Sort:</Divider>
+      <Cascader
+      className="home-task-srt"
+        style={{ width: '100%' }}
+        options={colors}
+        multiple
+        maxTagCount="responsive"
+        defaultValue={['All']}
+      />
+
+      <Divider orientation="left">Date</Divider>
+      <div className="side-page">
+        <Card className='defu-card'>
+          <Meta
+            title="Card title - 1"
+            description="02/02/2000 - 18.00 PM"
+          />
+          <Tag className='side-tag' color="blue">TA</Tag>
+          <Checkbox className='task-fsn' onChange={onChange}></Checkbox> 
+        </Card>
+
+        <Card className='defu-card'>
+          <Meta
+            title="Card title - 2" 
+            description="Dateline: 18.00 PM"
+          />
+          <Tag className='side-tag' color="red">High School</Tag>
+          <Checkbox className='task-fsn' onChange={onChange}></Checkbox> 
+        </Card>
+        
+      </div>
+
+      <Button className="new-task-button" type="defualt" >+ Add new Task</Button>
     </div>
   );
 }
