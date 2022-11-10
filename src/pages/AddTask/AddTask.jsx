2dfb1114d322644ba217ca21694871ee6fb42d57
klemonade <lemon.thanakrit@gmail.com>
2022-11-10 15:24:38 +0700

    [init]

--- src/pages/AddTask/AddTask.jsx
+++ src/pages/AddTask/AddTask.jsx
@@ -1,15 +1,113 @@
-import React from "react";
-import { Button, Upload, Cascader, Divider, Input, Tag, UploadOutlined, DatePicker, TimePicker } from "antd";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  Button,
+  Upload,
+  Cascader,
+  Divider,
+  Input,
+  Tag,
+  DatePicker,
+  TimePicker,
+  Tooltip,
+} from "antd";
 import TextArea from "antd/lib/input/TextArea";
-import './AddTask.css';
-import './CustDes.css';
+import "./AddTask.css";
+import "./CustDes.css";
+
+// import { tasks } from "./dummy";
 
 export function AddTask() {
+  const tasks = [
+    {
+      id: "udahfrghs9fhg", //DocRef.id
+      data: {
+        title: "Task 1",
+        content:
+          "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        deadline: new Date(),
+        reminder: new Date(),
+        tags: ["uni", "see"],
+        status: "ongoing",
+      },
+    },
+    {
+      id: "udahfrghs9fhg", //DocRef.id
+      data: {
+        title: "Task 1",
+        content:
+          "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        deadline: new Date(),
+        reminder: new Date(),
+        tags: ["uni", "see"],
+        status: "ongoing",
+      },
+    },
+    {
+      id: "udahfrghs9fhg", //DocRef.id
+      data: {
+        title: "Task 1",
+        content:
+          "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        deadline: new Date(),
+        reminder: new Date(),
+        tags: ["uni", "see"],
+        status: "ongoing",
+      },
+    },
+  ];
+
   const [task, setTask] = React.useState("");
   const [taskDes, setDes] = React.useState("");
   // const { MonthPicker, RangePicker, WeekPicker } = DatePicker;
-  const opt_remin = [{value:'',label:'None'}, {value:'30 min',label:'30 min'}, {value:'1 hours',label:'1 hours'}]
-  
+  const opt_remin = [
+    { value: "", label: "None" },
+    { value: "30 min", label: "30 min" },
+    { value: "1 hours", label: "1 hours" },
+  ];
+
+  const [tags, setTags] = useState([]);
+  const [inputVisible, setInputVisible] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [editInputIndex, setEditInputIndex] = useState(-1);
+  const [editInputValue, setEditInputValue] = useState("");
+  const inputRef = useRef(null);
+  const editInputRef = useRef(null);
+  useEffect(() => {
+    if (inputVisible) {
+      inputRef.current?.focus();
+    }
+  }, [inputVisible]);
+  useEffect(() => {
+    editInputRef.current?.focus();
+  }, [inputValue]);
+  const handleClose = (removedTag) => {
+    const newTags = tags.filter((tag) => tag !== removedTag);
+    console.log(newTags);
+    setTags(newTags);
+  };
+  const showInput = () => {
+    setInputVisible(true);
+  };
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleInputConfirm = () => {
+    if (inputValue && tags.indexOf(inputValue) === -1) {
+      setTags([...tags, inputValue]);
+    }
+    setInputVisible(false);
+    setInputValue("");
+  };
+  const handleEditInputChange = (e) => {
+    setEditInputValue(e.target.value);
+  };
+  const handleEditInputConfirm = () => {
+    const newTags = [...tags];
+    newTags[editInputIndex] = editInputValue;
+    setTags(newTags);
+    setEditInputIndex(-1);
+    setInputValue("");
+  };
 
   const handleChange = (e) => {
     setTask(e.target.value);
@@ -18,69 +116,162 @@ export function AddTask() {
     setDes(e.target.value);
   };
 
-  const handleClick = () => { 
-    console.log('Button clicked');
+  const handleClick = () => {
+    console.log("Button clicked");
     console.log(task);
     console.log(taskDes);
-  }
+  };
 
   function onChange(date, dateString) {
     console.log(date, dateString);
   }
 
   return (
-    <div className="add-task">
-      <div className="add-task-info">
-      <Divider orientation="left">Title</Divider>
-      <Input
-        className="add-task-name"
-        placeholder="Add task"
-        value={task}
-        onChange={handleChange}
-      />
-      
-      <Divider orientation="left">Tag</Divider>
-      <div className="add-task-tag">
-        <Tag color="magenta">magenta</Tag>
-        <Tag color="red">red</Tag>
-        <Tag color="green">green</Tag>
+    <div className="w-full p-8">
+      <div className="flex flex-col gap-2 max-w-sm mx-auto">
+        <div className="flex flex-row w-full items-center justify-between">
+          <text className="font-bold text-xl">Remind-เด้อ </text>
+          <Button
+            className="bg-red-700 text-white rounded-xl hover:bg-red-500 border-red-700 border-none hover:text-white"
+            type="primart"
+            onChange={onChange}
+          >
+            Logout
+          </Button>
+        </div>
+        <Divider orientation="left">Title</Divider>
+        <Input
+          className="w-full"
+          placeholder="Add task"
+          value={task}
+          onChange={handleChange}
+        />
+
+        <Divider orientation="left">Tag</Divider>
+        <div className="flex flex-row flex-wrap gap-2 border p-4">
+          {tags.map((tag, index) => {
+            if (editInputIndex === index) {
+              return (
+                <Input
+                  ref={editInputRef}
+                  key={tag}
+                  size="small"
+                  className="tag-input"
+                  value={editInputValue}
+                  onChange={handleEditInputChange}
+                  onBlur={handleEditInputConfirm}
+                  onPressEnter={handleEditInputConfirm}
+                />
+              );
+            }
+            const isLongTag = tag.length > 20;
+            const tagElem = (
+              <Tag
+                className="edit-tag"
+                key={tag}
+                closable
+                onClose={() => handleClose(tag)}
+              >
+                <span
+                  onDoubleClick={(e) => {
+                    if (index !== 0) {
+                      setEditInputIndex(index);
+                      setEditInputValue(tag);
+                      e.preventDefault();
+                    }
+                  }}
+                >
+                  {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                </span>
+              </Tag>
+            );
+            return isLongTag ? (
+              <Tooltip title={tag} key={tag}>
+                {tagElem}
+              </Tooltip>
+            ) : (
+              tagElem
+            );
+          })}
+          {inputVisible && (
+            <Input
+              ref={inputRef}
+              type="text"
+              size="small"
+              className="tag-input"
+              value={inputValue}
+              onChange={handleInputChange}
+              onBlur={handleInputConfirm}
+              onPressEnter={handleInputConfirm}
+            />
+          )}
+          {!inputVisible && (
+            <Tag className="site-tag-plus" onClick={showInput}>
+              New Tag +
+            </Tag>
+          )}
+        </div>
+
+        <Divider orientation="left">Deadline</Divider>
+        <div className="flex flex-row justify-around items-center">
+          <DatePicker className="dl-task-date" onChange={onChange} />
+          <TimePicker
+            className="dl-task-time"
+            use12Hours
+            format="h:mm A"
+            onChange={onChange}
+          />
+        </div>
+
+        <Divider orientation="left">Reminder</Divider>
+        <div className="flex flex-row justify-center items-center">
+          <Cascader
+            placeholder="Select Time"
+            options={opt_remin}
+            changeOnSelect
+          />
+        </div>
+        <Divider orientation="left">Description</Divider>
+        <span className="flex flex-row items-center justify-center gap-1 mt-0">
+          <Button className="str-bold">
+            <strong>B</strong>
+          </Button>
+          <Button className="str-em">
+            <em>I</em>
+          </Button>
+          <Button className="str-u">
+            <u>U</u>
+          </Button>
+        </span>
+
+        <TextArea
+          className="add-task-des"
+          // style={{ width: 337 }}
+          autoSize={{ minRows: 3, maxRows: 3 }}
+          placeholder="Add Description"
+          value={taskDes}
+          onChange={handleDes}
+        />
+        <div className="flex flex-col gap-2 justify-center items-center w-full">
+          <Upload className="flex flex-row items-center justify-center">
+            <Button className="add-task-upload"> Click to Upload </Button>
+          </Upload>
+          <Button
+            className="bg-red-700 hover:bg-red-500 text-white h-[50px] rounded-lg border-none w-full"
+            type="primary"
+            onClick={handleClick}
+          >
+            Cancel
+          </Button>
+          <Button
+            className="bg-black hover:bg-gray-800 h-[50px] rounded-lg border-none w-full"
+            type="primary"
+            onClick={handleClick}
+          >
+            Add
+          </Button>
+        </div>
       </div>
-
-      <Divider orientation="left">Deadline</Divider>
-      <div className="dl-task">
-        <DatePicker className="dl-task-date" onChange={onChange} />
-        <TimePicker className="dl-task-time" use12Hours format="h:mm A" onChange={onChange} />
-      </div>
-      
-      <Divider orientation="left">Reminder</Divider>
-      <Cascader 
-        placeholder="Select Time"
-        options={opt_remin} 
-        changeOnSelect 
-      />
-
-      <Divider orientation="left">Description</Divider>
-      <span className="str-cont-des">
-        <Button className="str-bold"><strong>B</strong></Button>
-        <Button className="str-em"><em>I</em></Button>
-        <Button className="str-u"><u>U</u></Button>
-      </span>
-
-      <TextArea
-        className="add-task-des"
-        // style={{ width: 337 }}
-        autoSize={{ minRows: 4, maxRows: 4 }}
-        placeholder="Add Description"
-        value={taskDes}
-        onChange={handleDes}
-      />
-      <Upload>
-        <Button className="add-task-upload"> <UploadOutlined /> Click to Upload </Button>
-      </Upload>
-      </div>
-      <Button className="cancel-button" type="defualt" onClick={handleClick} >Cancel</Button>
-      <Button className="add-button" type="defualt" onClick={handleClick} >Add</Button>
-
     </div>
   );
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,61 +1,236 @@
-import React from 'react';
-import { Divider, Cascader, Button, Card, Tag, Checkbox } from "antd";
-import './HomePage.css';
-
+import React, {useState, useRef, useEffect} from "react";
+import { Divider, Cascader, Button, Card, Tag, Checkbox, Input, Tooltip } from "antd";
+import "./HomePage.css";
+import { PresetColorTypes } from "antd/lib/_util/colors";
+import { PresetStatusColorTypes } from "antd/es/_util/colors";
 
 export function HomePage() {
-  const colors = [{value:'All',label:'All'}, {value:'TA',label:'TA'}, {value:'High School',label:'High School'}]
+  const tasks = [
+    {
+      id: "udahfrghs9fhg", //DocRef.id
+      data: {
+        title: "Task 1",
+        content:
+          "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        deadline: new Date(),
+        reminder: new Date(),
+        tags: ["uni", "see"],
+        status: "ongoing",
+      },
+    },
+    {
+      id: "udahfrghs9fhg", //DocRef.id
+      data: {
+        title: "Task 1",
+        content:
+          "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        deadline: new Date(),
+        reminder: new Date(),
+        tags: ["uni", "see"],
+        status: "ongoing",
+      },
+    },
+    {
+      id: "udahfrghs9fhg", //DocRef.id
+      data: {
+        title: "Task 1",
+        content:
+          "lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quod.",
+        deadline: new Date(),
+        reminder: new Date(),
+        tags: ["uni", "see"],
+        status: "ongoing",
+      },
+    },
+  ];
+  // const colors = [
+  //   { value: "All", label: "All" },
+  //   { value: "TA", label: "TA" },
+  //   { value: "High School", label: "High School" },
+  // ];
   const { Meta } = Card;
   const onChange = (e) => {
     console.log(`checked = ${e.target.checked}`);
   };
 
+  const [tags, setTags] = useState([]);
+  const [inputVisible, setInputVisible] = useState(false);
+  const [inputValue, setInputValue] = useState("");
+  const [editInputIndex, setEditInputIndex] = useState(-1);
+  const [editInputValue, setEditInputValue] = useState("");
+  const inputRef = useRef(null);
+  const editInputRef = useRef(null);
+  useEffect(() => {
+    if (inputVisible) {
+      inputRef.current?.focus();
+    }
+  }, [inputVisible]);
+  useEffect(() => {
+    editInputRef.current?.focus();
+  }, [inputValue]);
+  const handleClose = (removedTag) => {
+    const newTags = tags.filter((tag) => tag !== removedTag);
+    console.log(newTags);
+    setTags(newTags);
+  };
+  const showInput = () => {
+    setInputVisible(true);
+  };
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleInputConfirm = () => {
+    if (inputValue && tags.indexOf(inputValue) === -1) {
+      setTags([...tags, inputValue]);
+    }
+    setInputVisible(false);
+    setInputValue("");
+  };
+  const handleEditInputChange = (e) => {
+    setEditInputValue(e.target.value);
+  };
+  const handleEditInputConfirm = () => {
+    const newTags = [...tags];
+    newTags[editInputIndex] = editInputValue;
+    setTags(newTags);
+    setEditInputIndex(-1);
+    setInputValue("");
+  };
 
   return (
-    <div className="homepage">
-      <div className="text text-2xl" > 
-        <text>Remind-เด้อ </text>
-        <Button className='log-out-task' onChange={onChange}> <u>Logout</u> </Button>
+    <div className="w-full p-8 h-[100vh]">
+      <div className="flex flex-col max-w-sm mx-auto gap-6 h-full">
+        <div className="flex flex-row w-full items-center justify-between">
+          <text className="font-bold text-xl">Remind-เด้อ </text>
+          <Button
+            className="bg-red-700 text-white rounded-xl hover:bg-red-500 border-red-700 border-none hover:text-white"
+            type="primart"
+            onChange={onChange}
+          >
+            Logout
+          </Button>
+        </div>
+
+        <div className="flex flex-row justify-center items-center gap-2">
+          <Button>Pending</Button>
+          <Button>Finished</Button>
+          <Button>Recently Deleted</Button>
+        </div>
+        <div>
+          <Divider orientation="left">Filter</Divider>
+          <div className="flex flex-row flex-wrap gap-2 border p-4">
+            {tags.map((tag, index) => {
+              if (editInputIndex === index) {
+                return (
+                  <Input
+                    ref={editInputRef}
+                    key={tag}
+                    size="small"
+                    className="tag-input"
+                    value={editInputValue}
+                    onChange={handleEditInputChange}
+                    onBlur={handleEditInputConfirm}
+                    onPressEnter={handleEditInputConfirm}
+                  />
+                );
+              }
+              const isLongTag = tag.length > 20;
+              const tagElem = (
+                <Tag
+                  className="edit-tag"
+                  key={tag}
+                  closable
+                  onClose={() => handleClose(tag)}
+                >
+                  <span
+                    onDoubleClick={(e) => {
+                      if (index !== 0) {
+                        setEditInputIndex(index);
+                        setEditInputValue(tag);
+                        e.preventDefault();
+                      }
+                    }}
+                  >
+                    {isLongTag ? `${tag.slice(0, 20)}...` : tag}
+                  </span>
+                </Tag>
+              );
+              return isLongTag ? (
+                <Tooltip title={tag} key={tag}>
+                  {tagElem}
+                </Tooltip>
+              ) : (
+                tagElem
+              );
+            })}
+            {inputVisible && (
+              <Input
+                ref={inputRef}
+                type="text"
+                size="small"
+                className="tag-input"
+                value={inputValue}
+                onChange={handleInputChange}
+                onBlur={handleInputConfirm}
+                onPressEnter={handleInputConfirm}
+              />
+            )}
+            {!inputVisible && (
+              <Tag className="site-tag-plus" onClick={showInput}>
+                New Tag +
+              </Tag>
+            )}
+          </div>
+        </div>
+
+        <div>
+          <Divider orientation="left">Tasks</Divider>
+          <div className="w-full flex flex-col gap-2 items-center justify-center">
+            {tasks.map((task) => {
+              return (
+                <Card className="w-[90%] h-fit">
+                  <Meta
+                    title={task.data.title}
+                    description={`${
+                      task.data.deadline.getDate() +
+                      "/" +
+                      task.data.deadline.getMonth() +
+                      "/" +
+                      task.data.deadline.getFullYear()
+                    } - ${
+                      task.data.deadline.getHours() +
+                      ":" +
+                      task.data.deadline.getMinutes()
+                    }`}
+                  />
+                  {task.data.tags.map((tag) => { 
+                    return (
+                    <Tag
+                      className="side-tag"
+                      color={PresetStatusColorTypes[Math.round((Math.random() * 100), 0) % 13]}
+                    >
+                      {tag}
+                    </Tag>)
+                  })}
+
+                  <Checkbox className="task-fsn" onChange={onChange}></Checkbox>
+                  <text className="task-fn-mark" disabled>
+                    {" "}
+                    Mark as done{" "}
+                  </text>
+                </Card>
+              );
+            })}
+          </div>
+        </div>
+
+        <Button
+          className="box-border h-11 w-full inline-block px-6 py-2.5 bg-black text-white rounded-lg"
+          type="primary"
+        >
+          + Add new Task
+        </Button>
       </div>
-
-      <Divider orientation="left"></Divider>
-      <div className='text-base italic'> Welcome to "<b>Name</b>" Calendar! </div>
-      
-      <Divider orientation="left">Sort:</Divider>
-      <Cascader
-      className="home-task-srt"
-        style={{ width: '100%' }}
-        options={colors}
-        multiple
-        maxTagCount="responsive"
-        defaultValue={['All']}
-      />
-
-      <Divider orientation="left">Date</Divider>
-      <div className="side-page">
-        <Card className='defu-card'>
-          <Meta
-            title="Card title - 1"
-            description="02/02/2000 - 18.00 PM"
-          />
-          <Tag className='side-tag' color="blue">TA</Tag>
-          <Checkbox className='task-fsn' onChange={onChange} ></Checkbox>
-          <text className='task-fn-mark' disabled> Mark as done </text>
-        </Card>
-
-        <Card className='defu-card'>
-          <Meta
-            title="Card title - 2" 
-            description="Dateline: 18.00 PM"
-          />
-          <Tag className='side-tag' color="red">High School</Tag>
-          <Checkbox className='task-fsn' onChange={onChange}></Checkbox>
-          <text className='task-fn-mark' disabled> Mark as done </text> 
-        </Card>
-        
-      </div>
-
-      <Button className="new-task-button" type="defualt" >+ Add new Task</Button>
     </div>
   );
 }
